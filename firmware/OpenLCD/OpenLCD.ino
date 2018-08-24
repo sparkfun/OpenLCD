faea8b5d9f840d9929e20197f280bf1e406aaf29
Gaston Williams <fourstix@gmail.com>
2018-08-24 17:30:01 -0400

    Add logic to print pipe if second control chracter is read
    This allows the user to print a pipe control character to the display by escaping the control character as a double pipe

--- firmware/OpenLCD/OpenLCD.ino
+++ firmware/OpenLCD/OpenLCD.ino
@@ -267,7 +267,14 @@ void updateDisplay()
     {
       SerLCD.write(byte(incoming - 35)); //You write location zero to display customer char 0
     }
+    //If we get a second special setting character, then write it to the display 
+    //This allows us to print a pipe by escaping it as a double 
+    else if (incoming == SPECIAL_SETTING) {
+      SerLCD.write(incoming);
 
+      currentFrame[characterCount++] = incoming; //Record this character to the display buffer
+      if (characterCount == settingLCDwidth * settingLCDlines) characterCount = 0; //Wrap condition
+    }
     modeSetting = false;
   }
   else if (modeTWI == true)
